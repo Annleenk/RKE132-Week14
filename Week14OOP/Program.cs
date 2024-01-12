01c5c9ff35cc59a0bca8b882b51f272276013237
ann 🌼 <annleenk@gmail.com>
2024-01-12 16:44:17 +0200

    add week14

---
+++ Week14OOP/Program.cs
@@ -0,0 +1,61 @@
+﻿Console.WriteLine("Name your dog:");
+string myDogName = Console.ReadLine();
+
+Dog myDog = new Dog(myDogName);
+Dog friendsDog = new Dog("Rax"); //objects
+
+
+Console.WriteLine($"My dogs name is {myDog.Name}");
+Console.WriteLine($"My friends dogs name is {friendsDog.Name}");
+//Console.WriteLine($"Oh no! {myDog.Name} bit {friendsDog.Name}!");
+//myDog.Rename("Bad Rix");
+
+
+while(myDog.LevelOfHappiness != 3)
+{
+    myDog.Bark();
+}
+
+myDog.WagTail();
+Console.WriteLine($"{myDog.Name} is happy!");
+class Dog
+{
+    private string _name; //field
+    private int _levelOfHappiness;
+
+    //constructor
+    public Dog(string name) // name - laseb nime sisestada
+    {
+        _name = name;
+        _levelOfHappiness = 0;
+    }
+
+    //getter
+
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    public int LevelOfHappiness
+    {
+        get { return _levelOfHappiness;}
+    }
+
+    public void Rename(string newName)
+    {
+        _name = newName;
+        Console.WriteLine($"The dog has been renamed to {newName}");
+    }
+
+    public void Bark()
+    {
+        Console.WriteLine("Woof-woof!");
+        _levelOfHappiness++;
+    }
+
+    public void WagTail()
+    {
+        Console.WriteLine("Wiggle-wiggle!");
+    }
+}
